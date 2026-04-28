a0da5c14312d789b3bb43337351bcc1ae9b035d3
Gwendal D. <116562154+KasutoSoei@users.noreply.github.com>
2026-04-28 16:19:24 +0200

    create event test

--- app/__tests__/EventScreen.test.tsx
+++ app/__tests__/EventScreen.test.tsx
@@ -5,7 +5,7 @@ import * as eventService from '@/services/event';
 
 jest.mock('@/services/event');
 
-const sampleEvent = {
+const mockEvent = {
   id: 1,
   name: 'Soirée Test',
   location: 'Paris',
@@ -14,13 +14,14 @@ const sampleEvent = {
 
 describe('EventScreen', () => {
   beforeEach(() => {
-    (eventService.get_event as jest.Mock).mockResolvedValue([sampleEvent]);
+    (eventService.get_event as jest.Mock).mockResolvedValue([mockEvent]);
   });
 
   afterEach(() => {
     jest.clearAllMocks();
   });
 
+  // Start of the test
   it('fetches events and displays them', async () => {
     const { getByText } = render(<EventScreen />);
 
@@ -29,5 +30,21 @@ describe('EventScreen', () => {
     expect(getByText('Paris')).toBeTruthy();
 
     expect(getByText('10€')).toBeTruthy();
+  });
+});
+
+describe('EventScreen', () => {
+  beforeEach(() => {
+    (eventService.create_event as jest.Mock).mockResolvedValue([mockEvent]);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('creates a new event and display them', async () => {
+    const result = await eventService.create_event(mockEvent);
+
+    expect(result).toEqual([mockEvent]);
   });
 });
